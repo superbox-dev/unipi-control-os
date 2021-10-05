789770ea2876a22bc6f83131ca845fbba5585204
Michael Hacker <mh@superbox.one>
2021-10-05 21:15:44 +0200

    updated unipi tools

--- package/unipi_tools/unipi_tools.mk
+++ package/unipi_tools/unipi_tools.mk
@@ -24,16 +24,12 @@ endef
 define UNIPI_TOOLS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(BINFILES:%=$(@D)/src/%) -t $(TARGET_DIR)/opt/unipi/tools
 	$(INSTALL) -D -m 0644 $(@D)/src/unipi-target.map -t $(TARGET_DIR)/opt/unipi/data
-	
+	$(INSTALL) -D -m 0644 $(@D)/overlays/*.dtbo -t $(BINARIES_DIR)/rpi-firmware/overlays	
 	$(INSTALL) -D -m 644 $(@D)/unipi-common/etc/modprobe.d/neuron-blacklist.conf $(TARGET_DIR)/etc/modprobe.d/neuron-blacklist.conf
 	$(INSTALL) -D -m 644 $(@D)/unipi-common/etc/initramfs/modules.d/unipi $(TARGET_DIR)/etc/modules-load.d/unipi.conf
 	$(INSTALL) -D -m 644 $(@D)/unipi-common/udev/95-unipi-plc-devices.rules $(TARGET_DIR)/etc/udev/rules.d/95-unipi-plc-devices.rules
 	$(INSTALL) -D -m 644 $(@D)/unipi-common/udev/95-unipi-usb-serial.rules $(TARGET_DIR)/etc/udev/rules.d/95-unipi-usb-serial.rules
-	$(INSTALL) -D -m 644 $(UNIPI_TOOLS_PKGDIR)/unipiconfig $(TARGET_DIR)/opt/unipi/tools/unipiconfig
-endef
-
-define UNIPI_TOOLS_INSTALL_IMAGES_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/overlays/*.dtbo -t $(BINARIES_DIR)/rpi-firmware/overlays
+	$(INSTALL) -D -m 755 $(UNIPI_TOOLS_PKGDIR)unipiconfig $(TARGET_DIR)/opt/unipi/tools/unipiconfig
 endef
 
 define UNIPI_TOOLS_INSTALL_INIT_SYSTEMD
@@ -43,8 +39,8 @@ define UNIPI_TOOLS_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 $(@D)/unipi-common/systemd/system/unipigate.target $(TARGET_DIR)/usr/lib/systemd/system/unipigate.target
 	$(INSTALL) -D -m 644 $(@D)/unipi-common/systemd/system/unipispi.target $(TARGET_DIR)/usr/lib/systemd/system/unipispi.target
 	$(INSTALL) -D -m 644 $(@D)/unipi-common/etc/tmpfiles.d/cpufreq.conf $(TARGET_DIR)/etc/tmpfiles.d/cpufreq.conf
-	$(INSTALL) -D -m 644 $(UNIPI_TOOLS_PKGDIR)/systemd/hwclock.service $(TARGET_DIR)/usr/lib/systemd/system/hwclock.service
-	$(INSTALL) -D -m 644 $(UNIPI_TOOLS_PKGDIR)/systemd/unipiconfig.service $(TARGET_DIR)/usr/lib/systemd/system/unipiconfig.service
+	$(INSTALL) -D -m 644 $(UNIPI_TOOLS_PKGDIR)systemd/hwclock.service $(TARGET_DIR)/usr/lib/systemd/system/hwclock.service
+	$(INSTALL) -D -m 644 $(UNIPI_TOOLS_PKGDIR)systemd/unipiconfig.service $(TARGET_DIR)/usr/lib/systemd/system/unipiconfig.service
 endef
 
 $(eval $(generic-package))

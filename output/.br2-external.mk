26e80341616c53717899dd0f0a434ade46ef9c40
Michael Hacker <mh@superbox.one>
2022-02-14 21:03:17 -0800

    New project structure

---
+++ output/.br2-external.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file; DO NOT EDIT.
+#
+
+BR2_EXTERNAL ?= /home/mh/unipi-control-os/buildroot
+BR2_EXTERNAL_NAMES = 
+BR2_EXTERNAL_DIRS = 
+BR2_EXTERNAL_MKS = 
+
+BR2_EXTERNAL_NAMES += UNIPI
+BR2_EXTERNAL_DIRS += /home/mh/unipi-control-os/buildroot
+BR2_EXTERNAL_MKS += /home/mh/unipi-control-os/buildroot/external.mk
+export BR2_EXTERNAL_UNIPI_PATH = /home/mh/unipi-control-os/buildroot
+export BR2_EXTERNAL_UNIPI_DESC = Buildroot for UniPi boards

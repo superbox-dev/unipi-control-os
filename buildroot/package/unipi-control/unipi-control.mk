3a0f44baf1a77a76fff3f05c58c37adf64211776
Michael Hacker <mh@superbox.one>
2022-06-06 12:37:55 +0000

    Updated LTS version

--- buildroot/package/unipi-control/unipi-control.mk
+++ buildroot/package/unipi-control/unipi-control.mk
@@ -4,11 +4,20 @@
 #
 ################################################################################
 
-UNIPI_CONTROL_VERSION = 1.1.1
+UNIPI_CONTROL_VERSION = 1.2.0
 UNIPI_CONTROL_SOURCE = $(UNIPI_CONTROL_VERSION).tar.gz
 UNIPI_CONTROL_SITE = https://github.com/mh-superbox/unipi-control/archive/refs/tags
 UNIPI_CONTROL_SETUP_TYPE = setuptools
 UNIPI_CONTROL_LICENSE = MIT
 UNIPI_CONTROL_LICENSE_FILES = LICENCE
+
+define UNIPI_CONTROL_INSTALL_TARGET_CMDS
+	cp -rf $(@D)/src/unipi_control/installer/etc/unipi -t $(TARGET_DIR)/etc/unipi
+endef
+
+define UNIPI_CONTROL_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(@D)/src/unipi_control/installer/etc/systemd/system/unipi-control.service \
+	       	$(TARGET_DIR)/etc/systemd/system/unipi-control.service
+endef
 
 $(eval $(python-package))

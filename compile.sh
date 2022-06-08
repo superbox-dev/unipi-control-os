a4960358ff742e3c69f7735a82607558caa6b1ae
Michael Hacker <mh@superbox.one>
2022-06-08 18:26:33 +0000

    Cleanup

--- compile.sh
+++
@@ -1,8 +0,0 @@
-BR_SRC_DIR=../buildroot
-UNIPI_SRC_DIR=$(pwd)
-OUTPUT=$UNIPI_SRC_DIR/output
-
-cd $BR_SRC_DIR
-
-#make O=$OUTPUT BR2_EXTERNAL=
-make O=$OUTPUT BR2_EXTERNAL=$UNIPI_SRC_DIR/buildroot BR2_TARGET_GENERIC_HOSTNAME=unipi-1 clean all

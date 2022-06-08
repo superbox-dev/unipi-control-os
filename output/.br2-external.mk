b97fa3079f550d7a9f1ba811298e36ee8e48f1e7
Michael Hacker <mh@superbox.one>
2022-06-08 18:25:12 +0000

    Clean packages

---
+++ output/.br2-external.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file; DO NOT EDIT.
+#
+
+BR2_EXTERNAL ?= /home/mh/rpi-buildroot/buildroot
+BR2_EXTERNAL_NAMES = 
+BR2_EXTERNAL_DIRS = 
+BR2_EXTERNAL_MKS = 
+
+BR2_EXTERNAL_NAMES += UNIPI
+BR2_EXTERNAL_DIRS += /home/mh/rpi-buildroot/buildroot
+BR2_EXTERNAL_MKS += /home/mh/rpi-buildroot/buildroot/external.mk
+export BR2_EXTERNAL_UNIPI_PATH = /home/mh/rpi-buildroot/buildroot
+export BR2_EXTERNAL_UNIPI_DESC = Buildroot for UniPi boards

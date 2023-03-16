fed0fe0d4be15b4dfddf19f10da30d023e96427e
Michael Hacker <mh@superbox.one>
2023-03-16 05:04:14 +0100

    Fixed wrong paths

--- buildroot/package/unipi-control/unipi-control.mk
+++ buildroot/package/unipi-control/unipi-control.mk
@@ -15,13 +15,13 @@ UNIPI_CONTROL_SETUP_TYPE = setuptools
 
 define UNIPI_CONTROL_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(UNIPI_CONTROL_PKGDIR)etc/unipi/control.yaml $(TARGET_DIR)/etc/unipi/control.yaml
-	$(INSTALL) -D -m 0644 $(@D)/src/unipi_control/install/etc/unipi/control.yaml -t $(TARGET_DIR)/etc/unipi/control.yaml.example
-	$(INSTALL) -D -m 0644 $(@D)/src/unipi_control/install/etc/unipi/hardware/neuron/L203.yaml -t $(TARGET_DIR)/etc/unipi/hardware/neuron/L203.yaml
-	$(INSTALL) -D -m 0644 $(@D)/src/unipi_control/install/etc/unipi/hardware/extensions/Eastron_SDM120M.yaml -t $(TARGET_DIR)/etc/unipi/hardware/extensions/Eastron_SDM120M.yaml
+	$(INSTALL) -D -m 0644 $(@D)/src/unipi_control/installer/etc/unipi/control.yaml -t $(TARGET_DIR)/etc/unipi/control.yaml.example
+	$(INSTALL) -D -m 0644 $(@D)/src/unipi_control/installer/etc/unipi/hardware/neuron/L203.yaml -t $(TARGET_DIR)/etc/unipi/hardware/neuron/L203.yaml
+	$(INSTALL) -D -m 0644 $(@D)/src/unipi_control/installer/etc/unipi/hardware/extensions/Eastron_SDM120M.yaml -t $(TARGET_DIR)/etc/unipi/hardware/extensions/Eastron_SDM120M.yaml
 endef
 
 define UNIPI_CONTROL_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 $(@D)/src/unipi_control/install/etc/systemd/system/unipi-control.service $(TARGET_DIR)/usr/lib/systemd/system/unipi-control.service
+	$(INSTALL) -D -m 644 $(@D)/src/unipi_control/installer/etc/systemd/system/unipi-control.service $(TARGET_DIR)/usr/lib/systemd/system/unipi-control.service
 endef
 
 $(eval $(python-package))

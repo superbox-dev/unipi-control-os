84919df97ed364925e6c2ddd9b2f002c692cbed3
Michael Hacker <66108120+mh-superbox@users.noreply.github.com>
2023-03-16 12:18:15 +0100

    Update unipi-kernel.mk

--- buildroot/package/unipi-kernel/unipi-kernel.mk
+++ buildroot/package/unipi-kernel/unipi-kernel.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-UNIPI_KERNEL_VERSION = 1.72
+UNIPI_KERNEL_VERSION = 2.22
 UNIPI_KERNEL_SOURCE = $(UNIPI_KERNEL_VERSION).tar.gz
-UNIPI_KERNEL_SITE = https://github.com/UniPiTechnology/unipi-kernel/archive/refs/tags
+UNIPI_KERNEL_SITE = https://github.com/UniPiTechnology/unipi-kernel-modules/archive/refs/tags
 
 UNIPI_KERNEL_LICENSE = GPL-2.0
 UNIPI_KERNEL_LICENSE_FILES = COPYING
 
 UNIPI_KERNEL_DEPENDENCIES += linux
 
-UNIPI_KERNEL_MODULE_SUBDIRS = modules/unipi modules/rtc-unipi
+UNIPI_KERNEL_MODULE_SUBDIRS = modules/rtc-unipi modules/unipi-id modules/unipi-rfkill modules/unipi-mfd
 
 $(eval $(kernel-module))
 $(eval $(generic-package))

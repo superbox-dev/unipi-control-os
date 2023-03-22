c19e2780ad6579f6cfd81df7d7075397f0c30fa7
Michael Hacker <mh@superbox.one>
2023-03-22 21:26:53 +0000

    Refactor packages and added unipi-modbus-tools 2.14

--- buildroot/package/unipi-tools/opt/unipi/tools/unipiconfig.sh
+++ buildroot/package/unipi-tools/opt/unipi/tools/unipiconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Set UniPi Neuron platform detection
+# Set Unipi Neuron platform detection
 
 if ! [ -f "/sys/bus/i2c/devices/1-0057/eeprom" ] ; then
     echo "24c01 0x57" > /sys/bus/i2c/devices/i2c-1/new_device

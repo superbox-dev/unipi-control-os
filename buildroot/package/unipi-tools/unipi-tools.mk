a0e9a6720dfac399844d6d2dc41970b2285d6bf7
Michael Hacker <66108120+mh-superbox@users.noreply.github.com>
2023-03-23 09:43:18 +0100

    Fixed wrong dependency

--- buildroot/package/unipi-tools/unipi-tools.mk
+++ buildroot/package/unipi-tools/unipi-tools.mk
@@ -8,7 +8,7 @@ UNIPI_TOOLS_VERSION = 1.2.46
 UNIPI_TOOLS_SOURCE = $(UNIPI_TOOLS_VERSION).tar.gz
 UNIPI_TOOLS_SITE = https://github.com/UniPiTechnology/unipi-tools/archive/refs/tags
 
-UNIPI_TOOLS_DEPENDENCIES += libmodbus libtool unipi-kernel
+UNIPI_TOOLS_DEPENDENCIES += libmodbus libtool unipi-kernel-modules-v1
 
 MODBUS_LIBDIR = $(STAGING_DIR)/usr/lib
 BINFILES = unipi_tcp_server fwspi fwserial unipihostname unipicheck

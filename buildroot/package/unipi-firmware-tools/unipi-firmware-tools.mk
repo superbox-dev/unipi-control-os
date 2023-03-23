30ed40d0762614ee2ed564b1d4196d1f530d16f0
Michael Hacker <mh@superbox.one>
2023-03-23 04:59:18 +0100

    Added unipi-firmware-tools package

---
+++ buildroot/package/unipi-firmware-tools/unipi-firmware-tools.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# unipi-firmware-tools
+#
+################################################################################
+
+UNIPI_FIRMWARE_TOOLS_VERSION = 2.14
+UNIPI_FIRMWARE_TOOLS_SOURCE = unipi-firmware-tools_$(UNIPI_FIRMWARE_TOOLS_VERSION)~bullseye_arm64.deb
+UNIPI_FIRMWARE_TOOLS_SITE = https://repo.unipi.technology/debian/pool/main/u/unipi-tools
+UNIPI_FIRMWARE_TOOLS_LICENSE = GPL-3.0+
+
+BINFILES = fwspi fwserial
+
+define UNIPI_FIRMWARE_TOOLS_EXTRACT_CMDS
+    ar x $(UNIPI_FIRMWARE_TOOLS_DL_DIR)/$(UNIPI_FIRMWARE_TOOLS_SOURCE) --output $(@D)
+    $(TAR) xf $(@D)/data.tar.xz -C $(@D)
+endef
+
+define UNIPI_FIRMWARE_TOOLS_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/opt/unipi/tools/fwserial -t $(TARGET_DIR)/opt/unipi/tools
+endef
+
+define UNIPI_FIRMWARE_TOOLS_INSTALL_INIT_SYSTEMD
+    $(INSTALL) -D -m 0644 $(@D)/lib/systemd/system/unipifwupdate.service -t $(TARGET_DIR)/usr/lib/systemd/system
+endef
+
+$(eval $(generic-package))

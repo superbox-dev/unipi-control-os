4a6e424e39c3f8a5b62bab727c28c9a561f0cf1e
Michael Hacker <mh@superbox.one>
2023-04-01 04:39:54 +0000

    Revert to resize systemd script

--- buildroot/board/neuron-rpi3-64/boot.cmd
+++ buildroot/board/neuron-rpi3-64/boot.cmd
@@ -12,7 +12,7 @@ fi
 
 if test "${console}" = "ttyS0,115200"; then setenv console "both"; fi
 if test "${console}" = "display" || test "${console}" = "both"; then setenv consoleargs "console=tty1"; fi
-if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "console=ttyS0,115200n ${consoleargs}"; fi
+if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "console=ttyS0,115200 ${consoleargs}"; fi
 
 setenv bootargs "root=${rootfs} rootwait rootfstype=${rootfstype} ${consoleargs} loglevel=${verbosity} consoleblank=0 fsck.repair=yes 8250.nr_uarts=1 ${extraargs}"
 

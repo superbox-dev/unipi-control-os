7f851121a47fbc529099cb49fe1feafa2ebd2605
Michael Hacker <mh@superbox.one>
2023-04-04 22:21:09 +0200

    Added convert_disk_image_xz()

--- buildroot/board/neuron-rpi3-64/post-image.sh
+++ buildroot/board/neuron-rpi3-64/post-image.sh
@@ -3,6 +3,7 @@
 set -u
 set -e
 
+HDD_IMG="${BINARIES_DIR}/sdcard.img"
 BOOT_IMG="${BINARIES_DIR}/boot.vfat"
 ROOTFS_IMG="${BINARIES_DIR}/rootfs.ext4"
 BOOT_DATA="${BINARIES_DIR}/boot"
@@ -32,12 +33,11 @@ create_boot_image() {
 
 create_disk_mbr() {
   disk_layout="${BINARIES_DIR}/disk.layout"
-  hdd_img="${BINARIES_DIR}/sdcard.img"
   data_img="${BINARIES_DIR}/data.ext4"
   overlay_img="${BINARIES_DIR}/overlay.ext4"
 
-  rm -f "${hdd_img}"
-  truncate --size="1778MiB" "${hdd_img}"
+  rm -f "${HDD_IMG}"
+  truncate --size="1778MiB" "${HDD_IMG}"
 
   (
      echo "label: dos"
@@ -52,22 +52,28 @@ create_disk_mbr() {
      echo "overlay   : start=1578MiB,   size=100MiB,    type=83"            # Make a logical Linux partition
   ) > "${disk_layout}"
 
-  sfdisk "${hdd_img}" < "${disk_layout}"
+  sfdisk "${HDD_IMG}" < "${disk_layout}"
 
-  dd if="${BOOT_IMG}" of="${hdd_img}" conv=notrunc,sparse bs=512 seek=2048
-  dd if="${ROOTFS_IMG}" of="${hdd_img}" conv=notrunc,sparse bs=512 seek=81920
+  dd if="${BOOT_IMG}" of="${HDD_IMG}" conv=notrunc,sparse bs=512 seek=2048
+  dd if="${ROOTFS_IMG}" of="${HDD_IMG}" conv=notrunc,sparse bs=512 seek=81920
 
   rm -f "${data_img}"
   truncate --size="100MiB" "${data_img}"
   mkfs.ext4 "${data_img}"
-  dd if="${data_img}" of="${hdd_img}" conv=notrunc,sparse bs=512 seek=3436544
+  dd if="${data_img}" of="${HDD_IMG}" conv=notrunc,sparse bs=512 seek=3436544
 
   rm -f "${overlay_img}"
   truncate --size="100MiB" "${overlay_img}"
   mkfs.ext4 "${overlay_img}"
-  dd if="${overlay_img}" of="${hdd_img}" conv=notrunc,sparse bs=512 seek=3231744
+  dd if="${overlay_img}" of="${HDD_IMG}" conv=notrunc,sparse bs=512 seek=3231744
+}
+
+convert_disk_image_xz() {
+    rm -f "${HDD_IMG}.xz"
+    xz -3 -T0 "${HDD_IMG}"
 }
 
 pre_image
 create_boot_image
 create_disk_mbr
+convert_disk_image_xz

465eb975e53a0bf08fd25469efaad1c53324ae4e
Michael Hacker <66108120+mh-superbox@users.noreply.github.com>
2023-04-05 15:38:15 +0200

    Update os-overlay-setup.sh

--- buildroot/board/neuron-rpi3-64/rootfs_overlay/usr/libexec/os-overlay-setup.sh
+++ buildroot/board/neuron-rpi3-64/rootfs_overlay/usr/libexec/os-overlay-setup.sh
@@ -13,6 +13,14 @@ if [ ! -f /mnt/overlay/etc/hosts ]; then
   cp -fp /etc/hosts /mnt/overlay/etc/hosts
 fi
 
+if [ ! -f /mnt/overlay/etc/systemd/network/20-wired.network ]; then
+  cp -fp /etc/systemd/network/20-wired.network /mnt/overlay/etc/systemd/network/20-wired.network
+fi
+
+if [ ! -f /mnt/overlay/etc/systemd/network/25-wireless.network ]; then
+  cp -fp /etc/systemd/network/25-wireless.network /mnt/overlay/etc/systemd/network/25-wireless.network
+fi
+
 if [ ! -f /mnt/overlay/etc/systemd/timesyncd.conf ]; then
   mkdir -p /mnt/overlay/etc/systemd
   cp -fp /etc/systemd/timesyncd.conf /mnt/overlay/etc/systemd/timesyncd.conf

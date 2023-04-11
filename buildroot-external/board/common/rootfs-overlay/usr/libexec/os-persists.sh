9898441f134d0d1b2acf35fff090424200d558a4
Michael Hacker <mh@superbox.one>
2023-04-11 15:19:59 +0200

    Added persistant MACHINE_ID

---
+++ buildroot-external/board/common/rootfs-overlay/usr/libexec/os-persists.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+MACHINE_ID=$(cat /etc/machine-id)
+
+if [ "$(fw_printenv -n MACHINE_ID)" != "${MACHINE_ID}" ]; then
+  echo "[INFO] set machine-id to ${MACHINE_ID}"
+  fw_setenv MACHINE_ID "${MACHINE_ID}"
+else
+  echo "[INFO] machine-id is okay"
+fi
+

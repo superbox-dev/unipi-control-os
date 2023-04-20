3d3e7e7880292e021681d983689618a2ca3e8d93
Michael Hacker <mh@superbox.one>
2023-04-20 12:08:28 +0200

    Refactor os-tools

--- buildroot-external/board/common/rootfs-overlay/etc/profile.d/os-shell.sh
+++
@@ -1,4 +0,0 @@
-OS_SHELL="/usr/local/bin/os-shell"
-
-[ -f "${OS_SHELL}" ] || exit 1
-PYTHONUSERBASE=/usr/local ${OS_SHELL}
--- buildroot-external/board/common/rootfs-overlay/etc/profile.d/python.sh
+++ buildroot-external/board/common/rootfs-overlay/etc/profile.d/python.sh
@@ -1,2 +1,10 @@
+# Start os-shell on login.
+OS_SHELL="/usr/local/bin/os-shell"
+[ ! -f "${OS_SHELL}" ] || PYTHONUSERBASE=/usr/local ${OS_SHELL}
+
+# Set python user base to /usr/local.
+# pip install packages without venv to this location.
 export PYTHONUSERBASE=/usr/local
+
+# Set virtualenv path for Unipi Control develop environment.
 export PATH="${PATH}:/usr/local/opt/venv/unipi-control/bin"

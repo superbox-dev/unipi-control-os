caa3b3abf10bb7a2bef5f6fe6decc0294e6e465b
Michael Hacker <mh@superbox.one>
2023-04-29 13:19:40 +0200

    Added overlay for /etc

--- buildroot-external/board/common/rootfs-overlay/usr/libexec/os-overlay-setup.sh
+++ buildroot-external/board/common/rootfs-overlay/usr/libexec/os-overlay-setup.sh
@@ -6,32 +6,8 @@ OVERLAY="/mnt/overlay"
 
 overlay_paths=(
   "${OVERLAY}/etc"
-  "${OVERLAY}/etc/opkg"
-  "${OVERLAY}/etc/ssh"
-  "${OVERLAY}/etc/systemd/network/$NETWORK"
-  "${OVERLAY}/var/cache/opkg"
-  "${OVERLAY}/var/lib/opkg"
-)
-
-file_names=(
-  "adjtime"
-  "hostname"
-  "hosts"
-  "passwd"
-  "group"
-  "shadow"
-  "opkg/opkg.conf"
-  "systemd/timesyncd.conf"
-  "systemd/network/20-wired.network"
-  "systemd/network/25-wireless.network"
 )
 
 for overlay_path in "${overlay_paths[@]}"; do
   mkdir -p "${overlay_path}"
-done
-
-for file_name in "${file_names[@]}"; do
-  if [ ! -f "${OVERLAY}/etc/${file_name}" ]; then
-    cp -fp "/etc/${file_name}" "${OVERLAY}/etc/${file_name}"
-  fi
 done

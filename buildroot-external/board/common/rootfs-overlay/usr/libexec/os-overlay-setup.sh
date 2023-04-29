20cc319fc34590114fac2709ecf7a4f72c628af8
Michael Hacker <mh@superbox.one>
2023-04-29 21:23:21 +0200

    Update init-overlay.sh

--- buildroot-external/board/common/rootfs-overlay/usr/libexec/os-overlay-setup.sh
+++ buildroot-external/board/common/rootfs-overlay/usr/libexec/os-overlay-setup.sh
@@ -5,13 +5,11 @@ set -e
 OVERLAY="/mnt/overlay"
 
 overlay_paths=(
-  "${OVERLAY}/etc"
   "${OVERLAY}/home"
   "${OVERLAY}/root"
   "${OVERLAY}/opt/unipi"
   "${OVERLAY}/usr/local"
   "${OVERLAY}/var"
-  "${OVERLAY}/.work-etc"
   "${OVERLAY}/.work-home"
   "${OVERLAY}/.work-root"
   "${OVERLAY}/.work-opt-unipi"
--- buildroot-external/board/common/rootfs-overlay/usr/sbin/init-overlay.sh
+++ buildroot-external/board/common/rootfs-overlay/usr/sbin/init-overlay.sh
@@ -3,7 +3,8 @@
 echo "TEST OVERLAY"
 
 mount /dev/mmcblk0p5 /mnt/overlay
-mkdir -pv /mnt/overlay/{etc,.work-etc}
+mkdir -pv /mnt/overlay/etc
+mkdir -pv /mnt/overlay/.work-etc
 mount -t overlay -o lowerdir=/etc,upperdir=/mnt/overlay/etc,workdir=/mnt/overlay/.work-etc overlay /etc
 
 exec /sbin/init

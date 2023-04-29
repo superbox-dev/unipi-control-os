fdfe5216cc1296e0d46d3c1fec67c879036a8d84
Michael Hacker <mh@superbox.one>
2023-04-29 15:58:23 +0200

    Removed data

--- buildroot-external/board/common/post-build.sh
+++ buildroot-external/board/common/post-build.sh
@@ -35,7 +35,7 @@ Documentation: https://github.com/superbox-dev/unipi-control#readme
 EOL
 
 # Create mount point directories
-mkdir -pv "${TARGET_DIR}/mnt/"{boot,data,overlay}
+mkdir -pv "${TARGET_DIR}/mnt/"{boot,overlay}
 
 function setup_zsh() {
   sed -i '/^root:/s,:/bin/dash$,:/bin/zsh,' "${TARGET_DIR}/etc/passwd"

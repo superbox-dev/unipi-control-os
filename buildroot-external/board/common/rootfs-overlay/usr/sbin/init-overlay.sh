370cfed598a192c160df25ee15617eaf895bc1f7
Michael Hacker <mh@superbox.one>
2023-04-29 22:04:11 +0200

    Update init-overlay.sh

--- buildroot-external/board/common/rootfs-overlay/usr/sbin/init-overlay.sh
+++ buildroot-external/board/common/rootfs-overlay/usr/sbin/init-overlay.sh
@@ -1,10 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "TEST OVERLAY"
+DEVICE_OVERLAY="$(findfs PARTLABEL=overlay)"
+mount -t ext4 -o defaults,noatime,commit=30 "$DEVICE_OVERLAY" /mnt/overlay
 
-mount /dev/mmcblk0p5 /mnt/overlay
-mkdir -pv /mnt/overlay/etc
-mkdir -pv /mnt/overlay/.work-etc
-mount -t overlay -o lowerdir=/etc,upperdir=/mnt/overlay/etc,workdir=/mnt/overlay/.work-etc overlay /etc
+function setup_overlay() {
+  mkdir -p "/mnt/overlay/$1"
+  mkdir -p "/mnt/overlay/$2"
+
+  mount overlay -t overlay \
+    -o "lowerdir=/$1,upperdir=/mnt/overlay/$1,workdir=/mnt/overlay/$2" "/$1"
+}
+
+setup_overlay "etc" ".work-etc"
+setup_overlay "home" ".work-home"
+setup_overlay "opt/unipi" ".work-opt-unipi"
+setup_overlay "root" ".work-root"
+setup_overlay "usr/local" ".work-usr-local"
+setup_overlay "var" ".work-var"
 
 exec /sbin/init

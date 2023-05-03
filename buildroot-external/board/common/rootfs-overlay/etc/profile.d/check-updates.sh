2ee0ecd3d18aa1fe13e6243245126e678400baef
Michael Hacker <mh@superbox.one>
2023-05-03 20:48:44 +0200

    Updated check-updates.sh

--- buildroot-external/board/common/rootfs-overlay/etc/profile.d/check-updates.sh
+++ buildroot-external/board/common/rootfs-overlay/etc/profile.d/check-updates.sh
@@ -10,9 +10,11 @@ else
   LATEST_RELEASE=$(curl -s -L https://api.github.com/repos/mh-superbox/unipi-control/releases/latest | jq --raw-output '.tag_name')
   CURRENT_RELEASE=$(/usr/local/bin/unipi-control --version | cut -d " " -f 2)
 
-  if awk "BEGIN {exit !($LATEST_RELEASE > $CURRENT_RELEASE)}"; then
-      echo -n "${COLOR_YELLOW}Unipi Control${COLOR_RESET} update available:"
-      echo -n "${COLOR_BOLD_WHITE}Installed:${COLOR_RESET}  ${CURRENT_RELEASE}"
-      echo -n "${COLOR_BOLD_WHITE}Latest:${COLOR_RESET}     ${LATEST_RELEASE}\n"
+  if [ "${LATEST_RELEASE}" != "" ] && [ "${CURRENT_RELEASE}" != "" ]; then
+    if awk "BEGIN {exit !($LATEST_RELEASE > $CURRENT_RELEASE)}"; then
+        echo -n "${COLOR_YELLOW}Unipi Control${COLOR_RESET} update available:"
+        echo -n "${COLOR_BOLD_WHITE}Installed:${COLOR_RESET}  ${CURRENT_RELEASE}"
+        echo -n "${COLOR_BOLD_WHITE}Latest:${COLOR_RESET}     ${LATEST_RELEASE}\n"
+    fi
   fi
 fi

c40ffddc0924a4abd7a9caa13d3e35ea6e286009
Michael Hacker <mh@superbox.one>
2023-05-04 20:05:40 +0200

    Fixed check tag

--- .github/scripts/check-tag.sh
+++ .github/scripts/check-tag.sh
@@ -2,8 +2,7 @@
 
 ref_name="${GITHUB_REF_NAME}"
 tag_version="${ref_name/.dev/}"
-version="${{ inputs.version }}"
-os_version="${version/.dev${GITHUB_RUN_NUMBER}/}"
+os_version="${${1:-}/.dev${GITHUB_RUN_NUMBER}/}"
 
 if [ "${tag_version}" != "${os_version}" ]; then
   echo "Version number in Buildroot metadata does not match tag! (${tag_version} vs. ${os_version})."

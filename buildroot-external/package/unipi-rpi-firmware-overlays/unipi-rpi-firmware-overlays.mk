821c39ed184f506472d9969510813140a73d0db1
Michael Hacker <mh@superbox.one>
2023-05-05 20:53:43 +0200

    Added unipi rpi firmware overlays package

---
+++ buildroot-external/package/unipi-rpi-firmware-overlays/unipi-rpi-firmware-overlays.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# unipi-rpi-firmware-overlays
+#
+################################################################################
+
+UNIPI_RPI_FIRMWARE_OVERLAYS_VERSION = 1.2.46
+UNIPI_RPI_FIRMWARE_OVERLAYS_SOURCE = $(UNIPI_RPI_FIRMWARE_OVERLAYS_VERSION).tar.gz
+UNIPI_RPI_FIRMWARE_OVERLAYS_SITE = https://github.com/UniPiTechnology/unipi-tools/archive/refs/tags
+
+UNIPI_RPI_FIRMWARE_OVERLAYS_DEPENDENCIES += unipi-kernel
+
+define UNIPI_RPI_FIRMWARE_OVERLAYS_BUILD_CMDS
+	cd $(@D)/overlays; $(MAKE) $(TARGET_CONFIGURE_OPTS) LINUX_DIR_PATH=$(LINUX_DIR)
+endef
+
+define UNIPI_RPI_FIRMWARE_OVERLAYS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/overlays/*.dtbo -t $(BINARIES_DIR)/rpi-firmware/overlays
+endef
+
+$(eval $(generic-package))

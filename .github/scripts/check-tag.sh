082095930aa3706fd8ad4f7d66de0a1bffbb717f
Michael Hacker <mh@superbox.one>
2023-05-05 23:21:23 +0200

    Updated CI for new contribution schema

--- .github/scripts/check-tag.sh
+++ .github/scripts/check-tag.sh
@@ -4,7 +4,7 @@ set -e
 
 os_version="v${1}"
 
-if [ "${GITHUB_REF_NAME}" != "${os_version}" ]; then
+if [ "${GITHUB_REF_TYPE}" == "tag" ] && [ "${GITHUB_REF_NAME}" != "${os_version}" ]; then
   echo "Version number in Buildroot metadata does not match tag! (${GITHUB_REF_NAME} vs. ${os_version})."
   exit 1
 fi

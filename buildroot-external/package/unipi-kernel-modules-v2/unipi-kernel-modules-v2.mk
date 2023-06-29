fd42fd8568d2e3bc147d9cb74de3504bd706aa3a
Michael Hacker <66108120+mh-superbox@users.noreply.github.com>
2023-06-29 10:57:06 +0200

    Update unipi-kernel-modules-v2.mk

--- buildroot-external/package/unipi-kernel-modules-v2/unipi-kernel-modules-v2.mk
+++ buildroot-external/package/unipi-kernel-modules-v2/unipi-kernel-modules-v2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UNIPI_KERNEL_MODULES_V2_VERSION = 2.22
+UNIPI_KERNEL_MODULES_V2_VERSION = 2.24
 UNIPI_KERNEL_MODULES_V2_SOURCE = $(UNIPI_KERNEL_MODULES_V2_VERSION).tar.gz
 UNIPI_KERNEL_MODULES_V2_SITE = https://github.com/UniPiTechnology/unipi-kernel-modules/archive/refs/tags
 UNIPI_KERNEL_MODULES_V2_LICENSE = GPL-2.0

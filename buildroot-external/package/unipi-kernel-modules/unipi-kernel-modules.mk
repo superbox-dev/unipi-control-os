57baf30eaf0e9f70ebe86499b1a793099a1b67b0
Michael Hacker <66108120+mh-superbox@users.noreply.github.com>
2023-07-03 12:59:35 +0200

    Update unipi-kernel-modules.mk

--- buildroot-external/package/unipi-kernel-modules/unipi-kernel-modules.mk
+++ buildroot-external/package/unipi-kernel-modules/unipi-kernel-modules.mk
@@ -13,7 +13,7 @@ UNIPI_KERNEL_MODULES_DEPENDENCIES += linux
 UNIPI_KERNEL_MODULES_MODULE_SUBDIRS = modules/rtc-unipi modules/unipi modules/unipi-id modules/unipi-rfkill
 
 define UNIPI_KERNEL_MODULES_EXTRACT_CMDS
-	$(TAR) xf $(UNIPI_KERNEL_MODULES_DL_DIR)/$(UNIPI_KERNEL_MODULES_SOURCE) -C $(@D)
+	$(AR) x --output $(@D) $(UNIPI_KERNEL_MODULES_DL_DIR)/$(UNIPI_KERNEL_MODULES_SOURCE)
 	$(TAR) xf $(@D)/data.tar.gz
 	mkdir $(@D)/modules
 	mv $(@D)/usr/share/unipi-$(UNIPI_KERNEL_MODULES_VERSION)/* $(@D)/modules

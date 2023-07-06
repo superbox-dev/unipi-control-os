c61f8fae23bb65bf2bbf7f70663f0d3c6a9f3005
Michael Hacker <mh@superbox.one>
2023-07-06 23:27:10 +0200

    Updates unipi-kernel-modules.mk to own kernel repo

--- buildroot-external/package/unipi-kernel-modules/unipi-kernel-modules.mk
+++ buildroot-external/package/unipi-kernel-modules/unipi-kernel-modules.mk
@@ -5,18 +5,19 @@
 ################################################################################
 
 UNIPI_KERNEL_MODULES_VERSION = 1.124
-UNIPI_KERNEL_MODULES_SOURCE = unipi-kernel-modules-dkms_$(UNIPI_KERNEL_MODULES_VERSION)_all.deb
-UNIPI_KERNEL_MODULES_SITE = https://repo.unipi.technology/debian/pool/main/u/unipi-kernel-modules
+UNIPI_KERNEL_MODULES_SOURCE = v$(UNIPI_KERNEL_MODULES_VERSION).tar.gz
+UNIPI_KERNEL_MODULES_SITE = https://github.com/superbox-dev/unipi-kernel/archive/refs/tags
 UNIPI_KERNEL_MODULES_LICENSE = GPL-3.0+
 
 UNIPI_KERNEL_MODULES_DEPENDENCIES += linux
 UNIPI_KERNEL_MODULES_MODULE_SUBDIRS = modules/rtc-unipi modules/unipi modules/unipi-id modules/unipi-rfkill
 
-define UNIPI_KERNEL_MODULES_EXTRACT_CMDS
-	$(AR) x --output $(@D) $(UNIPI_KERNEL_MODULES_DL_DIR)/$(UNIPI_KERNEL_MODULES_SOURCE)
-	$(TAR) xf $(@D)/data.tar.xz -C $(@D)
-	mkdir $(@D)/modules
-	mv $(@D)/usr/src/unipi-$(UNIPI_KERNEL_MODULES_VERSION)/* $(@D)/modules
+define UNIPI_KERNEL_MODULES_BUILD_CMDS
+	cd $(@D)/overlays; $(MAKE) $(TARGET_CONFIGURE_OPTS) LINUX_DIR_PATH=$(LINUX_DIR)
+endef
+
+define UNIPI_KERNEL_MODULES_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/overlays/*.dtbo -t $(BINARIES_DIR)/rpi-firmware/overlays
 endef
 
 $(eval $(kernel-module))

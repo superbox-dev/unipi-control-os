38d3107e308157c3cec0e50fce041a4b0c95fb94
Michael Hacker <mh@superbox.one>
2023-07-06 23:16:44 +0200

    Added licence to unipi-kernel-modules.mk

--- buildroot-external/package/unipi-kernel-modules/unipi-kernel-modules.mk
+++ buildroot-external/package/unipi-kernel-modules/unipi-kernel-modules.mk
@@ -7,9 +7,9 @@
 UNIPI_KERNEL_MODULES_VERSION = 1.124
 UNIPI_KERNEL_MODULES_SOURCE = unipi-kernel-modules-dkms_$(UNIPI_KERNEL_MODULES_VERSION)_all.deb
 UNIPI_KERNEL_MODULES_SITE = https://repo.unipi.technology/debian/pool/main/u/unipi-kernel-modules
+UNIPI_KERNEL_MODULES_LICENSE = GPL-3.0+
 
 UNIPI_KERNEL_MODULES_DEPENDENCIES += linux
-
 UNIPI_KERNEL_MODULES_MODULE_SUBDIRS = modules/rtc-unipi modules/unipi modules/unipi-id modules/unipi-rfkill
 
 define UNIPI_KERNEL_MODULES_EXTRACT_CMDS

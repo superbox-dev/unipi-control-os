3b600e46e92ebd914c87e16b7e4f651c596e44f8
Michael Hacker <mh@superbox.one>
2023-07-08 05:20:58 +0200

    Bump unipi kernel modules to v2.26

--- buildroot-external/package/unipi-kernel-modules/unipi-kernel-modules.mk
+++ buildroot-external/package/unipi-kernel-modules/unipi-kernel-modules.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-UNIPI_KERNEL_MODULES_VERSION = 1.124
+UNIPI_KERNEL_MODULES_VERSION = 2.26
 UNIPI_KERNEL_MODULES_SOURCE = v$(UNIPI_KERNEL_MODULES_VERSION).tar.gz
 UNIPI_KERNEL_MODULES_SITE = https://github.com/superbox-dev/unipi-kernel/archive/refs/tags
 UNIPI_KERNEL_MODULES_LICENSE = GPL-3.0+
 
 UNIPI_KERNEL_MODULES_DEPENDENCIES += linux
-UNIPI_KERNEL_MODULES_MODULE_SUBDIRS = modules/rtc-unipi modules/unipi modules/unipi-id modules/unipi-rfkill
+UNIPI_KERNEL_MODULES_MODULE_SUBDIRS = modules/rtc-unipi modules/unipi-id modules/unipi-mfd modules/unipi-rfkill
 
-define UNIPI_KERNEL_MODULES_BUILD_CMDS
-	cd $(@D)/overlays; $(MAKE) $(TARGET_CONFIGURE_OPTS) LINUX_DIR_PATH=$(LINUX_DIR)
-endef
+#define UNIPI_KERNEL_MODULES_BUILD_CMDS
+#	cd $(@D)/overlays; $(MAKE) $(TARGET_CONFIGURE_OPTS) LINUX_DIR_PATH=$(LINUX_DIR)
+#endef
 
 define UNIPI_KERNEL_MODULES_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/overlays/*.dtbo -t $(BINARIES_DIR)/rpi-firmware/overlays
